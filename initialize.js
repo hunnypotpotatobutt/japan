18c4f844965eb4b42f831d73f89965e3d90de607
hunnypotpotatobutt <159744208+hunnypotpotatobutt@users.noreply.github.com>
2024-02-16 09:00:05 -0500

    Add files via upload

--- initialize.js
+++ initialize.js
@@ -1,17 +1,466 @@
 unicornGundamStatue = {
-    latitude: 35.6260691780958,
-    longitude: 139.77534831101303,
+    latitude: 35.6242338,
+    longitude: 139.774279,
     map: 'https://www.google.com/maps/place/Unicorn+Gundam+Statue/@35.6327364,139.8191797,13.83z/data=!4m6!3m5!1s0x601889f9ca53ac17:0xe235a48a3479cb53!8m2!3d35.624451!4d139.7754917!16s%2Fg%2F11fq8sd4fb?entry=ttu',
+    tags: 'attraction',
+    hours: '7p+',
+    rating: 4.6,
+    review: 13766
 }
-var touristAttractions = ["unicornGundamStatue"];
-var touristAttractionsTitles = [];
+marunochiBuilding = {
+    latitude: 35.681620,
+    longitude: 139.7637458,
+    map: 'https://www.google.com/maps/place/Marunouchi+Building/@35.6816206,139.7637458,17.04z/data=!4m6!3m5!1s0x60188bf992c34823:0x21ed577a75a4b0d1!8m2!3d35.6810403!4d139.7638027!16s%2Fm%2F0gxzbtk?entry=ttu',
+    tags: 'mall',
+    hours: '11a-11p',
+    rating: 4,
+    review: 10776
+}
+tokyoMidtownHibiya = {
+    latitude: 35.6737436,
+    longitude: 139.7591762,
+    map: 'https://www.google.com/maps/search/?cid=0x60188bee3cf5818b:0x2c4beff9dc43a879',
+    tags: 'mall',
+    hours: '11a-11p',
+    rating: 3.9,
+    review: 9835
+}
+futakoTamagawaRiseShoppingCenter = {
+    latitude: 35.61175,
+    longitude: 139.6282344,
+    map: 'https://www.google.com/maps?ftid=0x6018f43fdf955555:0x7d8ca1a1c8966b96',
+    tags: 'mall',
+    hours: '10a-8p',
+    rating: 3.9,
+    review: 9578
+}
+tokyoDaijingu = {
+    latitude: 35.6999858,
+    longitude: 139.746891,
+    map: 'https://www.google.com/maps/search/?cid=0x60188c43ddbaaaab:0x7223e2de290136ef',
+    tags: 'attraction',
+    hours: '6a-9p',
+    rating: 4.4,
+    review: 9500
+}
+heavensGate = {
+    latitude: 35.6980094,
+    longitude: 139.7724164,
+    map: 'https://www.google.com/maps/search/?cid=0x60188c1d5176cd1f:0x62ff60d5d8b654fd',
+    tags: 'maidCafe',
+    hours: '11a-11p*',
+    rating: 4.9,
+    review: 9400
+}
+universalStudiosJapan = {
+    latitude: 34.6656768,
+    longitude: 135.4323185,
+    map: 'https://www.google.com/maps?ftid=0x6000e0d083d5e25d:0x3605fe25303252aa',
+    tags: 'themePark',
+    hours: '8a-10p*',
+    rating: 4.5,
+    review: 111019
+}
+tokyoDisneyland = {
+    latitude: 35.6328964,
+    longitude: 139.8803943,
+    map: 'https://www.google.com/maps?ftid=0x60187d03114737b3:0xe4d93636d509d3cb',
+    tags: 'themePark',
+    hours: '8a-10p',
+    rating: 4.6,
+    review: 101904
+}
+tokyoSkytree = {
+    latitude: 35.7100627,
+    longitude: 139.8107004,
+    map: 'https://www.google.com/maps?ftid=0x60188ed0d12f9adf:0x7d1d4fb31f43f72a',
+    tags: 'observation',
+    hours: '10a-9p',
+    rating: 4.4,
+    review: 81700
+}
+sensoJi = {
+    latitude: 35.7147651,
+    longitude: 139.7966553,
+    map: 'https://www.google.com/maps/search/?cid=0x60188ec1a4463df1:0x6c0d289a8292810d',
+    tags: 'shrine',
+    hours: '',
+    rating: 4.5,
+    review: 70700
+}
+tokyoTower = {
+    latitude: 35.6585805,
+    longitude: 139.7454329,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJCewJkL2LGGAR3Qmk0vCTGkg',
+    tags: 'observationDeck',
+    hours: '9a-10:30p',
+    rating: 4.4,
+    review: 70000
+}
+fushimiInariTaisha = {
+    latitude: 34.9676945,
+    longitude: 135.7791876,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJIW0uPRUPAWAR6eI6dRzKGns',
+    tags: 'shrine, hike',
+    hours: '24/7',
+    rating: 4.6,
+    review: 66200
+}
+osakaCastle = {
+    latitude: 34.6872571,
+    longitude: 135.5258546,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJ_TooXM3gAGARQR6hXH3QAQ8',
+    tags: 'castle,museum',
+    hours: '',
+    rating: 4.4,
+    review: 65800
+}
+dotonbori = {
+    latitude: 34.6687234,
+    longitude: 135.5012971,
+    map: 'https://www.google.com/maps/search/?cid=0x6000e713818af9fd:0xb8bb1326b48ba590',
+    tags: 'shopping,streetFood,streetPerformers,nightlife,ferrisWheel,boatRide',
+    hours: '24/7',
+    rating: 4.4,
+    review: 65200
+}
+okinawaChuraumiAquarium = {
+    latitude: 26.694338,
+    longitude: 127.8780131,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJPZ5hUjH65DQR_p_dD3CmCOo',
+    tags: 'aquarium',
+    hours: '8:30a-6:30p',
+    rating: 4.6,
+    review: 54841
+}
+naraPark = {
+    latitude: 34.685047,
+    longitude: 135.843012,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJYWCMvZY5AWARVnREV_OsbPk',
+    tags: 'deer, park',
+    hours: '24/7',
+    rating: 4.5,
+    review: 54571
+}
+kiyomizuDera = {
+    latitude: 34.9946662,
+    longitude: 135.784661,
+    map: 'https://www.google.com/maps/search/?cid=0x600108d385dcfb07:0x62af658650c434ba',
+    tags: 'architecture, temple',
+    hours: '6a-6p',
+    rating: 4.5,
+    review: 49671
+}
+kinkakuJi = {
+    latitude: 35.03937,
+    longitude: 135.7292431,
+    map: 'https://www.google.com/maps?ftid=0x6001a820c0eb46bd:0xee4272b1c22645f',
+    tags: 'architecture, temple',
+    hours: '9a-5p',
+    rating: 4.5,
+    review: 48908
+}
+tsukijiOuterMarket = {
+    latitude: 35.666348,
+    longitude: 139.7693612,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJW2cLzSGLGGARXAKXv6EkbqI',
+    tags: 'seafoodMarket',
+    hours: '',
+    rating: 4.3,
+    review: 48847
+}
+abenoHarukas = {
+    latitude: 34.6460706,
+    longitude: 135.5134771,
+    map: 'https://www.google.com/maps?ftid=0x6000ddf023e6dd79:0x76f9200a45c2127',
+    tags: 'observationDeck,museum,shopping,dining',
+    hours: '9a-10p',
+    rating: 4.2,
+    review: 46751
+}
+himejiCastle = {
+    latitude: 34.839449,
+    longitude: 134.6939047,
+    map: 'https://www.google.com/maps/search/?cid=0x3554e003a23324b3:0x7a4f8c2f6eba81b1',
+    tags: 'castle',
+    hours: '9a-4p',
+    rating: 4.6,
+    review: 46157
+}
+canalCityHakata = {
+    latitude: 33.5897944,
+    longitude: 130.4111028,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJYcOBiZWRQTUR0Rl0ehe67eA',
+    tags: 'mall,ghibli,architecture',
+    hours: '10a-9p',
+    rating: 4.1,
+    review: 41407
+}
+yokohamaChinatown = {
+    latitude: 35.4434459,
+    longitude: 139.6460909,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJ__-Le-9cGGARNY-CTSHwq5A',
+    tags: 'chinatown',
+    hours: '24/7',
+    rating: 4,
+    review: 41825
+}
+osakaAquariumKaiyukan = {
+    latitude: 34.6545182,
+    longitude: 135.4289645,
+    map: 'https://www.google.com/maps?ftid=0x6000e8f48c0da9cd:0x6f83c520ae082ccc',
+    tags: 'aquarium',
+    hours: '10a-8p',
+    rating: 4.4,
+    review: 41338
+}
+tokyoDome = {
+    latitude: 35.7056396,
+    longitude: 139.7518913,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJ89TugkeMGGARDmSeJIiyWFA',
+    tags: 'events',
+    hours: '',
+    rating: 4.2,
+    review: 40605
+}
+osakaCastlePark = {
+    latitude: 34.6864797,
+    longitude: 135.5262114,
+    map: 'https://www.google.com/maps/search/?cid=0x6000e0c977655555:0x67f69eaef984d98b',
+    tags: 'park',
+    hours: '24/7',
+    rating: 4.3,
+    review: 38352
+}
+shinjukuGyoenNationalGarden = {
+    latitude: 35.6851763,
+    longitude: 139.710051,
+    map: 'https://www.google.com/maps?ftid=0x60188cc21b93233f:0x6a1eb1b5a117f287',
+    tags: 'park, playground',
+    hours: '9a-4p',
+    rating: 4.6,
+    review: 36181
+}
+yokohamaRedBrickWarehouse = {
+    latitude: 35.4522967,
+    longitude: 139.6429481,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJSXGAhfhcGGARcz3MKth9lJQ',
+    tags: 'food, shopping, iceSkating, events',
+    hours: '11a-8p',
+    rating: 4.1,
+    review: 36361
+}
+meijiJingu = {
+    latitude: 35.6763976,
+    longitude: 139.6993259,
+    map: 'https://www.google.com/maps?ftid=0x60188cb79a4c26e5:0x8fca893849103f73',
+    tags: 'shrine, forest',
+    hours: '',
+    rating: 4.6,
+    review: 35448
+}
+nishikiMarket = {
+    latitude: 35.0050258,
+    longitude: 135.764723,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJT8uMzZwIAWARnGzsARCjnrY',
+    tags: 'foodMarket',
+    hours: '',
+    rating: 4.2,
+    review: 34855
+}
+tokyoSolamachiMise = {
+    latitude: 35.7102333,
+    longitude: 139.8115747,
+    map: 'https://www.google.com/maps?ftid=0x60188ed6f9ddd30f:0x918ba47a310c8809',
+    tags: 'mall',
+    hours: '10a-9p',
+    rating: 4.1,
+    review: 34678
+}
+roppongiHills = {
+    latitude: 35.6602384,
+    longitude: 139.7300767,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJM9xJEHeLGGARAshMWUgC_ls',
+    tags: 'observationDeck, museum, shopping, dining',
+    hours: '24/7',
+    rating: 4.1,
+    review: 34678
+}
+umihotaruPA = {
+    latitude: 35.5168462,
+    longitude: 139.8359258,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJC1xKH8hxGGARziJHXAbFqFw',
+    tags: 'restStop, shopping, foodCourt',
+    hours: '24/7',
+    rating: 4,
+    review: 34774
+}
+okageYokocho = {
+    latitude: 34.4624294,
+    longitude: 136.7228539,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJL93NSZNQBGARmNlxBX-cj9Y',
+    tags: 'shops, street',
+    hours: '9:30a-5p',
+    rating: 4.2,
+    review: 34148
+}
+nagoyaCastle = {
+    latitude: 35.1847501,
+    longitude: 136.8996883,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJse-wx8t2A2ARd6Z1knjp07k',
+    tags: 'castle',
+    hours: '9a-4:30p',
+    rating: 4.1,
+    review: 33986
+}
+tokyoBigSight = {
+    latitude: 35.6321411,
+    longitude: 139.7974638,
+    map: 'https://www.google.com/maps?ftid=0x601889dc629d1e7b:0xa4d1509a76045a01',
+    tags: 'events',
+    hours: '8a-8p',
+    rating: 3.9,
+    review: 33917
+}
+nijoCastle = {
+    latitude: 35.0140379,
+    longitude: 135.7484258,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJC5srCtQHAWARLy9qkFmHaxA',
+    tags: 'castle, architecture',
+    hours: '8:45a-5p',
+    rating: 4.4,
+    review: 33030
+}
+uenoZoologicalGardens = {
+    latitude: 35.7160153,
+    longitude: 139.772882,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJf2129yiMGGAR9I6Yoe-1uHY',
+    tags: 'zoo',
+    hours: '9:30a-5p',
+    rating: 4.3,
+    review: 32355
+}
+dazaifuTenmangu = {
+    latitude: 33.5213697,
+    longitude: 130.5348239,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJzfvrKp-bQTURl65fp6vRJLg',
+    tags: 'shrine',
+    hours: '6a-7:30p',
+    rating: 4.4,
+    review: 32372
+}
+oshinoHakkai = {
+    latitude: 35.4600675,
+    longitude: 138.8324741,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJK4SZn4VmGWARcZ-vsGukOhY',
+    tags: 'village',
+    hours: '24/7',
+    rating: 4.2,
+    review: 31402
+}
+kenrokuEn = {
+    latitude: 35.2013187,
+    longitude: 135.9024363,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJBVmy-YMz-F8R5PID8D17Cpc',
+    tags: 'park',
+    hours: '',
+    rating: 4.4,
+    review: 30647
+}
+shirakawaGo = {
+    latitude: 36.2577967,
+    longitude: 136.9061975,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJ5yW_trBx-F8R-AVYnbtRxcw',
+    tags: 'village',
+    hours: '',
+    rating: 4.5,
+    review: 31319
+}
+hankyuUmedaMainStore = {
+    latitude: 34.7028186,
+    longitude: 135.4985323,
+    map: 'https://www.google.com/maps?ftid=0x6000e692589cb9eb:0xbc9b6d5116f447ad',
+    tags: 'departmentStore',
+    hours: '10a-8p',
+    rating: 4.1,
+    review: 30703
+}
+tsutenkaku = {
+    latitude: 34.652499,
+    longitude: 135.5063058,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJ_0Lgd2DnAGARV0X03lbPy-U',
+    tags: 'observationDeck',
+    hours: '10a-8p',
+    rating: 4,
+    review: 29664
+}
+kaminarimon = {
+    latitude: 35.7111163,
+    longitude: 139.7963656,
+    map: 'https://www.google.com/maps?ftid=0x60188ec6db068cd1:0xd9a856805c8012bd',
+    tags: 'temple',
+    hours: '24/7',
+    rating: 4.5,
+    review: 29065
+}
+diverCityTokyoPlaza = {
+    latitude: 35.6251856,
+    longitude: 139.7756314,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJpbpu0_mJGGARd9sJ-psh9Gc',
+    tags: 'mall',
+    hours: '10a-9p*',
+    rating: 4.2,
+    review: 28683
+}
+atomicBombDome = {
+    latitude: 34.395483,
+    longitude: 132.453592,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJqYAn2wyiWjURlsDG4Hpn5jQ',
+    tags: 'memorial',
+    hours: '24/7',
+    rating: 4.7,
+    review: 27739
+}
+itsukushimaJinja = {
+    latitude: 34.2959896,
+    longitude: 132.3198285,
+    map: 'https://www.google.com/maps/search/?q=place_id:ChIJj23HfgTjGmARkNW10PcocjU',
+    tags: 'shrine',
+    hours: '6:30a-6p',
+    rating: 4.6,
+    review: 27614
+}
+var places = ["unicornGundamStatue","tokyoDaijingu","marunochiBuilding","tokyoMidtownHibiya","futakoTamagawaRiseShoppingCenter","tokyoSkytree","sensoJi","tokyoTower","fushimiInariTaisha",
+"heavensGate","universalStudiosJapan","tokyoDisneyland","osakaCastle","dotonbori","okinawaChuraumiAquarium","naraPark","kiyomizuDera","kinkakuJi","tsukijiOuterMarket","abenoHarukas","himejiCastle"
+,"canalCityHakata","yokohamaChinatown","osakaAquariumKaiyukan","tokyoDome","osakaCastlePark","shinjukuGyoenNationalGarden","yokohamaRedBrickWarehouse","meijiJingu","nishikiMarket","tokyoSolamachiMise","roppongiHills",
+"umihotaruPA","okageYokocho","nagoyaCastle","tokyoBigSight","nijoCastle","uenoZoologicalGardens","dazaifuTenmangu","oshinoHakkai","kenrokuEn","shirakawaGo","hankyuUmedaMainStore","tsutenkaku",
+"kaminarimon","diverCityTokyoPlaza","atomicBombDome","tsutenkaku","tsutenkaku","tsutenkaku","tsutenkaku","tsutenkaku","tsutenkaku","tsutenkaku","tsutenkaku","tsutenkaku",];
+var placesTitles = [];
+var placesHTML = [];
+var placesDistance = [];
 
-for (var i = 0; i < touristAttractions.length; i++) {    
-    const text = touristAttractions[i];
+for (var i = 0; i < places.length; i++) {    
+    const text = places[i];
     const result = text.replace(/([A-Z])/g, " $1");
-    touristAttractionsTitles[i] = result.charAt(0).toUpperCase() + result.slice(1);
+    placesTitles[i] = result.charAt(0).toUpperCase() + result.slice(1);
+    placesDistance[i] = (distance(35.61, 139.62, window[places[i]].latitude, window[places[i]].longitude)*0.621371).toFixed(2);
     
-    document.getElementById("tourist attractions").innerHTML += '<br /><small id='+touristAttractions[i]+'>'+touristAttractionsTitles[i]+'</small>'+
-    '<a href="'+window[touristAttractions[i]].map+'" target="_blank"><img src=”map.png”></a>'+'<img src=”map.png”>';
-    console.log(window[touristAttractions[i]].map);
+    placesHTML[i] = '<br /><a href="'+window[places[i]].map+'" target="_blank"><small class = "placeText" id='+places[i]+'>'+placesTitles[i]+'</small></a>'+ '<small> '+window[places[i]].hours+' '+placesDistance[i]+'mi</small>';
+    if (window[places[i]].tags.includes('night')) {placesHTML[i] += '<img src = "moon.png" style = "height:16px;width:16px">'}
+    
+}
+
+for (var i = 0; i < placesHTML.length; i++) {   
+    document.getElementById("places").innerHTML += placesHTML[i];
+}
+
+function distance(lat1, lon1, lat2, lon2) {
+  const r = 6371; // km
+  const p = Math.PI / 180;
+
+  const a = 0.5 - Math.cos((lat2 - lat1) * p) / 2
+                + Math.cos(lat1 * p) * Math.cos(lat2 * p) *
+                  (1 - Math.cos((lon2 - lon1) * p)) / 2;
+
+  return 2 * r * Math.asin(Math.sqrt(a));
 }
